16dfb27f7d7ef7f28d0f8f49847269fd0ae8e7b0
Gabriele Rocha <gabrielerocha17@outlook.com>
2025-02-03 21:56:32 -0300

    Implementação da arquitetura e Estrutura dos Dados - Em andamento

---
+++ src/main/java/com/gabriele/todolist/model/Task.java
@@ -0,0 +1,29 @@
+package com.gabriele.todolist.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = false)
+    private String status;
+}
